bc2e8ebf78d5d12bafe0050dd071e3f64bb4209a
justinmorales1 <jm.em0210@gmail.com>
2021-07-07 20:19:25 -0500

    Cleaning up repositoriesList

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -13,13 +13,17 @@ const RepositoriesList: React.FC = () => {
 
     searchRepositories(term)
   }
-console.log(data)
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={term} onChange={e => setTerm(e.target.value)}/>
         <button type="submit">Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+      data.map((name) => <div key={name}> {name}</div>)}
     </div>
   )
 };
