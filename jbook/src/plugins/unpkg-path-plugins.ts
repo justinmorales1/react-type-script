de5b7ff63f4c69f480f998e1ec9ff9d9c6118e41
justinmorales1 <jm.em0210@gmail.com>
2021-07-09 11:12:24 -0500

    Refactoring unpkg plugin file

--- jbook/src/plugins/unpkg-path-plugins.ts
+++ jbook/src/plugins/unpkg-path-plugins.ts
@@ -1,54 +1,67 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios'; 
+import localForage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
+
+export const unpkgPathPlugin = (input: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of the index.js file
+      build.onResolve({filter: /(^index\.js$)/}, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative paths in a module.
+      build.onResolve({filter: /^\.+\//}, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL (args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+        };
+      })
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace:'a'};
-        } 
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL (args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-          }
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
-        }
-        // else if ( args.path === 'tiny-test-pkg') {
-        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //            namespace:'a'}
-        // }
-        
+        };
       });
  
+    
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
  
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
-            `,
+            contents: input
           };
-        } 
+        } ;
 
-        const { data, request } = await axios.get(args.path);
-        console.log(request);
-        return {
+        // Check to see if we have already fetch this file and 
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        
+        //if it is in the cache, return it.
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);     
+
+        const result: esbuild.OnLoadResult =  {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
+        // Store response in cache.
+        await fileCache.setItem(args.path, result);
+
+        return result;
 
       });
     },
