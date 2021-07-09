45b40dd6e2104734c2fdc288344c2f74541b4305
justinmorales1 <jm.em0210@gmail.com>
2021-07-09 12:47:19 -0500

    Removing wasm dependency

--- jbook/src/plugins/unpkg-path-plugins.ts
+++ jbook/src/plugins/unpkg-path-plugins.ts
@@ -1,37 +1,31 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'; 
-import localForage from 'localforage';
 
-const fileCache = localForage.createInstance({
-  name: 'filecache'
-});
-
-
-export const unpkgPathPlugin = (input: string) => {
+export const unpkgPathPlugin = () => {
   return {
+
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      // Handle root entry file of the index.js file
-      build.onResolve({filter: /(^index\.js$)/}, () => {
+      // Handle root entry file of index.js
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return { path: 'index.js', namespace: 'a' };
       });
 
       // Handle relative paths in a module.
-      build.onResolve({filter: /^\.+\//}, (args: any) => {
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
           namespace: 'a',
-          path: new URL (args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-        };
-      })
-
-      // Handle main file of a module
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        return {
-          namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
         };
       });
 
-    }
+      // Handle main file of a module.
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
   };
 };
