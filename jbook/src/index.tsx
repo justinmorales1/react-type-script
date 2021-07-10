f77418dc139d683987bc3952084f057e7b66756c
justinmorales1 <jm.em0210@gmail.com>
2021-07-10 10:45:27 -0500

    Fixing files

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,9 +1,8 @@
 import * as esbuild from 'esbuild-wasm';
-import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 import { fetchPlugin } from './plugins/fetch-plugin';
-
 
 const App = () => {
   const ref = useRef<any>();
@@ -13,24 +12,17 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https;//unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
-    
-  }
-
-  useEffect(()=> {
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
     startService();
-  },[])
+  }, []);
 
   const onClick = async () => {
-    if(!ref.current) {
+    if (!ref.current) {
       return;
     }
-
-    // const result = await ref.current.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015'
-    // });
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -38,27 +30,33 @@ const App = () => {
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE_ENV' : '"production"',
-        global: 'window'
-      }
-    })
-  
-    setCode(result.outputFiles[0].text)
-  }
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  const html = `
+    <script>
+      ${code}
+    </script>
+  `;
 
   return (
     <div>
-      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} />
     </div>
-  )
+  );
 };
 
-
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
--- jbook/src/plugins/unpkg-path-plugins.ts
+++ jbook/src/plugins/unpkg-path-plugins.ts
@@ -2,15 +2,14 @@ import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
   return {
-
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      // Handle root entry file of index.js
+      // Handle root entry file of 'index.js'
       build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return { path: 'index.js', namespace: 'a' };
       });
 
-      // Handle relative paths in a module.
+      // Handle relative paths in a module
       build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
           namespace: 'a',
@@ -19,7 +18,7 @@ export const unpkgPathPlugin = () => {
         };
       });
 
-      // Handle main file of a module.
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
