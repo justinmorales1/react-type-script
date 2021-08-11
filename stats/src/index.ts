e0439b6dcfe61076066d50452e2445d12a4781f1
justinmorales1 <jm.em0210@gmail.com>
2021-08-11 13:56:40 -0500

    Added some example codes with abstract classes and interfaces

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,19 @@
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new CsvFileReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+
 
 let manUnitedWins = 0;
 
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D"
-}
 
-
-
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if(match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
