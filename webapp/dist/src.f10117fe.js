e477b2c9cbec45b75b3432170f743ac930128ec4
justinmorales1 <jm.em0210@gmail.com>
2021-08-13 11:17:15 -0500

    Adding usershow, useredit, and view

--- webapp/dist/src.f10117fe.js
+++ webapp/dist/src.f10117fe.js
@@ -117,39 +117,26 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/views/UserForms.ts":[function(require,module,exports) {
+})({"src/views/View.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.UserForm = void 0;
+exports.View = void 0;
 
-var UserForm = function () {
-  function UserForm(parent, model) {
-    var _this = this;
-
+var View = function () {
+  function View(parent, model) {
     this.parent = parent;
     this.model = model;
-
-    this.onSetAgeClick = function () {
-      _this.model.setRandomAge();
-    };
-
-    this.onSetNameClick = function () {
-      var input = _this.parent.querySelector('input');
-
-      var name = input.value;
-
-      _this.model.set({
-        name: name
-      });
-    };
-
     this.bindModel();
   }
 
-  UserForm.prototype.bindModel = function () {
+  View.prototype.eventsMap = function () {
+    return {};
+  };
+
+  View.prototype.bindModel = function () {
     var _this = this;
 
     this.model.on("change", function () {
@@ -157,18 +144,7 @@ var UserForm = function () {
     });
   };
 
-  UserForm.prototype.eventsMap = function () {
-    return {
-      'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNameClick
-    };
-  };
-
-  UserForm.prototype.template = function () {
-    return "\n      <div> \n        <h1> User Form </h1>\n        <div> User name: " + this.model.get('name') + "</div>\n        <div> User age: " + this.model.get('age') + "</div>\n        <input />\n        <button class=\"set-name\"> Change Name </button>\n        <button class=\"set-age\"> Set Random Age </button>\n      </div>\n    ";
-  };
-
-  UserForm.prototype.bindEvents = function (fragment) {
+  View.prototype.bindEvents = function (fragment) {
     var eventsMap = this.eventsMap();
 
     var _loop_1 = function _loop_1(eventKey) {
@@ -186,7 +162,7 @@ var UserForm = function () {
     }
   };
 
-  UserForm.prototype.render = function () {
+  View.prototype.render = function () {
     this.parent.innerHTML = '';
     var templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
@@ -194,11 +170,94 @@ var UserForm = function () {
     this.parent.append(templateElement.content);
   };
 
-  return UserForm;
+  return View;
 }();
 
+exports.View = View;
+},{}],"src/views/UserForms.ts":[function(require,module,exports) {
+"use strict";
+
+var __extends = this && this.__extends || function () {
+  var _extendStatics = function extendStatics(d, b) {
+    _extendStatics = Object.setPrototypeOf || {
+      __proto__: []
+    } instanceof Array && function (d, b) {
+      d.__proto__ = b;
+    } || function (d, b) {
+      for (var p in b) {
+        if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+      }
+    };
+
+    return _extendStatics(d, b);
+  };
+
+  return function (d, b) {
+    if (typeof b !== "function" && b !== null) throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+
+    _extendStatics(d, b);
+
+    function __() {
+      this.constructor = d;
+    }
+
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+  };
+}();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.UserForm = void 0;
+
+var View_1 = require("./View");
+
+var UserForm = function (_super) {
+  __extends(UserForm, _super);
+
+  function UserForm() {
+    var _this = _super !== null && _super.apply(this, arguments) || this;
+
+    _this.onSaveClick = function () {
+      _this.model.save();
+    };
+
+    _this.onSetAgeClick = function () {
+      _this.model.setRandomAge();
+    };
+
+    _this.onSetNameClick = function () {
+      var input = _this.parent.querySelector('input');
+
+      if (input) {
+        var name = input.value;
+
+        _this.model.set({
+          name: name
+        });
+      }
+    };
+
+    return _this;
+  }
+
+  UserForm.prototype.eventsMap = function () {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
+    };
+  };
+
+  UserForm.prototype.template = function () {
+    return "\n      <div> \n      \n        <input placeholder=\"" + this.model.get('name') + "\"/>\n        <button class=\"set-name\"> Change Name </button>\n        <button class=\"set-age\"> Set Random Age </button>\n        <button class=\"save-model\"> Save User </button>\n      </div>\n    ";
+  };
+
+  return UserForm;
+}(View_1.View);
+
 exports.UserForm = UserForm;
-},{}],"src/models/Model.ts":[function(require,module,exports) {
+},{"./View":"src/views/View.ts"}],"src/models/Model.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -2310,8 +2369,14 @@ var user = User_1.User.buildUser({
   name: "Stewie",
   age: 11
 });
-var userForm = new UserForms_1.UserForm(document.getElementById('root'), user);
-userForm.render();
+var root = document.getElementById('root');
+
+if (root) {
+  var userForm = new UserForms_1.UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('The root element was not found');
+}
 },{"./views/UserForms":"src/views/UserForms.ts","./models/User":"src/models/User.ts"}],"../../../../.nvm/versions/node/v12.3.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -3,8 +3,13 @@ import { User } from "./models/User";
 
 const user = User.buildUser({name: "Stewie", age: 11})
 
-const userForm = new UserForm(
-  document.getElementById('root'), user
-);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root,user);
+  userForm.render();
+} else {
+  throw new Error('The root element was not found');
+}
+
+
