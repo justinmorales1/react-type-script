0175d004fc35aaf03b1a8d72974709cd8fd14b32
justinmorales1 <jm.em0210@gmail.com>
2021-08-13 16:12:10 -0500

    Adding login routes

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        return next(); // This doesnt actually return anthing.
+        return; // Not returning anything
+    }
+    res.status(403);
+    res.send("AAAHHHHH AHHHHHH AAHHHHHH.....You didn't say the magic word.");
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -9,9 +17,26 @@ router.get('/login', function (req, res) {
 });
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    if (email && password && email === "justin" && password === " password") {
+    if (email && password && email === "justin" && password === "password") {
+        req.session = { loggedIn: true };
+        res.redirect('/');
     }
     else {
         res.send("Invalid email or password");
     }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n      <div>\n        <div> You are logged in </div>\n        <a href=\"/logout\"> Logout </a>\n      </div>\n    ");
+    }
+    else {
+        res.send("\n    <div>\n      <div> You are not logged in </div>\n      <a href=\"/login\"> Login </a>\n    </div>\n  ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send("Welcome to the good route");
 });
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response} from "express";
+import { Router, Request, Response, NextFunction} from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined}
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if(req.session && req.session.loggedIn) {
+    return next(); // This doesnt actually return anthing.
+    return; // Not returning anything
+  }
+
+  res.status(403);
+  res.send("AAAHHHHH AHHHHHH AAHHHHHH.....You didn't say the magic word.");
 }
 
 const router = Router();
@@ -29,16 +39,44 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password }  = req.body;
 
 
-  if (email && password && email === "justin" && password === " password") {
-    
+  if (email && password && email === "justin" && password === "password") {
+    req.session = { loggedIn: true}
+    res.redirect('/')
   } else {
     res.send("Invalid email or password");
   }
-
 });
 
 
 
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div> You are logged in </div>
+        <a href="/logout"> Logout </a>
+      </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div> You are not logged in </div>
+      <a href="/login"> Login </a>
+    </div>
+  `);
+  }
+});
+
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to the good route");
+})
 
 
 export { router };
