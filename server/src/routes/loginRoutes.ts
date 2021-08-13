b6773fc61f299168180bc40bc655af412e4bea0a
justinmorales1 <jm.em0210@gmail.com>
2021-08-13 15:51:48 -0500

    Adding cookie session and new routes

---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,44 @@
+import { Router, Request, Response} from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined}
+}
+
+const router = Router();
+
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST"> 
+      <div>
+        <label>Email</label>
+        <input name="email"/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password"/>
+        <button> Submit </button>
+      </div>
+    </form>
+  
+  `);
+});
+
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password }  = req.body;
+
+
+  if (email && password && email === "justin" && password === " password") {
+    
+  } else {
+    res.send("Invalid email or password");
+  }
+
+});
+
+
+
+
+
+export { router };
