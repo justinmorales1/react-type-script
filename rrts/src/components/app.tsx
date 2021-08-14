ee97b05fe07088bfe81d0a7aa9d71ab3bb4a514f
justinmorales1 <jm.em0210@gmail.com>
2021-08-14 13:01:15 -0500

    Adding loading state to show an example with a constructor

--- rrts/src/components/app.tsx
+++ rrts/src/components/app.tsx
@@ -1,23 +1,62 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function; //This is because its a redux thunk and it will cause an error.
+  deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
-  componentDidMount() {
+
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false}
+  }
+  // componentDidMount() {
+  //   this.props.fetchTodos();
+  // }
+
+
+  componentDidUpdate(prevProps: AppProps) : void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({fetching: false});
+    }
+  }
+
+  onButtonClick = () => {
     this.props.fetchTodos();
+    this.setState({fetching: true})
+  }
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+          <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+            {todo.title}
+          </div>
+      );
+    });
   }
 
   render() {
+    console.log(this.props.todos)
     return (
       <div>
-        
+        <button onClick={this.onButtonClick}> Fetch</button>
+        {this.state.fetching ? 'Loading': null}
+        {this.renderList()}
       </div>
     )
   }
@@ -29,4 +68,4 @@ const mapStateToProps = (state: StoreState) : {todos: Todo[]} => {
   }
 }
 
-export const App = connect(mapStateToProps,{fetchTodos})(_App);
+export const App = connect(mapStateToProps,{fetchTodos, deleteTodo})(_App);
--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,12 +1,14 @@
-import { Todo, FetchTodosAction } from '../actions';
-import { ActionTypes } from '../actions/types';
+import { Todo, ActionTypes, Actions } from '../actions';
 
-export const todosReducer = (state: Todo[] =  [], action: FetchTodosAction) => {
+
+export const todosReducer = (state: Todo[] =  [], action: Actions) => {
   switch(action.type) {
     case ActionTypes.fetchTodos:
       return action.paylod;
     // case ActionTypes.fetchTodos:
     //   return 5;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
